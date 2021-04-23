578c778b83afd75c843c133c3b1738e59dcc84ad
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-22 20:44:15 -0400

    updated href alts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,9 +11,9 @@ import Nav from "react-bootstrap/Nav"
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">About Me</Nav.Link>
-            <Nav.Link href="#pricing">Portfolio</Nav.Link>
-            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav.Link href="#AboutMe">About Me</Nav.Link>
+            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
