0e3ae54d07edd696902cf64178e72fdfb0c53c1f
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-25 15:12:23 -0400

    Updated About me summary:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import css from "./components/Header/Header.css"
+import MainPage from './components/MainPage/MainPage';
+import MAINCSS from './components/MainPage/MainCSS.css';
+import ContactForm from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+
 
 function App() {
   return (
     <div>
 
     <Header />
+    <MainPage />
+    <ContactForm />
+   
    
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 
+
  function Header() {
     
     return (
         <div>
-            <Navbar id="navbar" collapseOnSelect expand="lg" bg="light" variant="light">
+             <Navbar id="navbar" collapseOnSelect expand="lg" bg="light" variant="light">
         <Navbar.Brand id="aboutmename" href="#home">Teangelo Burks</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -16,9 +17,9 @@ import Nav from "react-bootstrap/Nav"
             <Nav.Link href="#Contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
+      </Navbar> 
         </div>
     )
 }
 
-export default Header
+export default Header;
