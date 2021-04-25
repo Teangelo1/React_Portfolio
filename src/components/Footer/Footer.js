7dc1dbc2395c0e7a3d6ea561113741cd45ce037f
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-25 16:33:59 -0400

    Added Footer code with styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer text-center fixed-bottom">Copyright. Teangelo Burks 2020-2021 </footer>
+    )
+}
