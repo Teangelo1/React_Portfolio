3262f0bb708ba4e37b9d6fc7b5f31d03c2d7ebe3
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-28 17:30:12 -0400

    updated images folder for portfolio, added blur effect to background image

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,31 +8,32 @@ import { AiFillFileImage } from 'react-icons/ai'
 import { MdEmail } from 'react-icons/md'
 
 export default function About() {
-    return (
-      <>
-        <Jumbotron className="jumbotron bg-transparent" fluid>
-  <Container>
-    <h1 id="CodingExp">Coding Experiment</h1>
-    <p id="jumboText">
-    My name is Teangelo Burks, a Full Stack Software Developer located in Michigan. 
-    I've always had a passion for designing aesthetically pleasing websites and applications. 
-    Luckily, Development has allowed me to express my creativity through many coding languages, frameworks, & technologies. During my studies of web development, I've learned to love both Frontend & Backend development equally. I have a natural curiosity for technology, and look forward to continuing my studies day after day.
+  return (
+    <>
+      <Jumbotron className="jumbotron bg-transparent" fluid>
+        <Container>
+          <h1 id="CodingExp">Coding Experiment</h1>
+          <p id="jumboText">
+            My name is Teangelo Burks, a Full Stack Software Developer located in Michigan.
+            I've always had a passion for designing aesthetically pleasing websites and applications.
+            Luckily, Development has allowed me to express my creativity through many coding languages, frameworks, & technologies. During my studies of web development, I've learned to love both Frontend & Backend development equally. I have a natural curiosity for technology, and look forward to continuing my studies day after day.
     </p>
-  </Container>
-</Jumbotron>
-<div className="iconCenter">
-<a href='https://www.linkedin.com/in/teangelo-burks-9516a2164/' target="#">
-<FaLinkedin /> </a>
+        </Container>
+      </Jumbotron>
+      <div className="iconCenter">
+        <a href='https://www.linkedin.com/in/teangelo-burks-9516a2164/' target="#">
+          <FaLinkedin /> </a>
 
-<a href="https://github.com/Teangelo1" target="#">
-<FaGithub /> 
-</a>
+        <a href="https://github.com/Teangelo1" target="#">
+          <FaGithub />
+        </a>
 
-<AiFillFileImage />
+        <a href="../Images/Teangelo Burks Resume.pdf" target="#"><AiFillFileImage />
+        </a>
 
-<a href="mailto:burks.teangelo@gmail.com" target="#">
-<MdEmail/> </a>
-</div>
-</>
-    )
+        <a href="mailto:burks.teangelo@gmail.com" target="#">
+          <MdEmail /> </a>
+      </div>
+    </>
+  )
 }
