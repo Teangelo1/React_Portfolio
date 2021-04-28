218259456ee5d53cdb2fc7ff3862c1d9e6829957
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-28 16:05:01 -0400

    modified navbar, removed about me link, adjusted portfolio link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,13 +8,13 @@ import './Header.css'
     
     return (
         <div>
-             <Navbar id="navbar" collapseOnSelect expand="lg" bg="transparent" variant="light">
+             <Navbar id="navbar" collapseOnSelect expand="lg" bg="transparent" variant="" className="navitemcolor">
         <Navbar.Brand id="aboutmename" href="/">Teangelo Burks</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto text-center move-left">
-            <Nav.Link className="text-center" href="/">About Me</Nav.Link>
-            <Nav.Link  href="/portfolio">Portfolio</Nav.Link>
+        <Navbar.Collapse id="responsive-navbar-nav collapsecolor">
+          <Nav className=" move-right">
+            {/* <Nav.Link className=" move-right" href="/">About Me</Nav.Link> */}
+            <Nav.Link className="move-right" href="/portfolio">Portfolio</Nav.Link>
             {/* <Nav.Link href="/contact">Contact</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
