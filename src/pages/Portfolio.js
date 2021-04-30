92b04a04f53ee64d289b0cd7541aa5c2ee84acf9
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-30 13:25:21 -0400

    Updated card names to match project title

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,92 +4,100 @@ import 'aos/dist/aos.css';
 import '../styles/Portfolio.css';
 import amnesiaNull from '../Images/amnesiaNull.png';
 import BudgetControl from '../Images/BudgetControl.png';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
+import restaurant from '../Images/restaurant.png'
+import passwordGen from '../Images/passwordGen.png'
+import CodingQuiz from '../Images/CodingQuiz.png'
 import "../styles/Portfolio.css";
-// import * as ReactBootstrap from "react-bootstrap";
+import Fitness from '../Images/Fitness.jpg';
 AOS.init();
 
 export default function Portfolio() {
   return (
     <>
      <header class="main-header">
-      <h1><span>Acrylic Painting</span> Gallery</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-        quam!
-      </p>
+      <h1><span>Project</span> Gallery</h1>
     </header>
 
     <main class="container">
       <section class="card">
-        <img src="https://i.ibb.co/VvC0vpN/paint-1.png" alt="" />
+        <img src={(restaurant)} alt="" />
         <div>
-          <h3>Acrylic Painting One</h3>
+          <h3>Fooood!</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
             enim laudantium, animi veniam libero eveniet culpa unde perferendis
             illo fugit corporis, voluptatibus totam dolorum, maiores magnam
             officia. Ab, delectus.
           </p>
-          <a href="#" class="btn">Buy Now</a>
+          <a href="#" class="btn">View Project</a>
         </div>
       </section>
 
       <section class="card" data-aos="fade-left">
-        <img src="https://i.ibb.co/3NHjDcW/paint-2.png" alt="" />
+        <img src={(amnesiaNull)} alt="" />
         <div>
-          <h3>Acrylic Painting Two</h3>
+          <h3>Amnesia(Null)</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+          An Application that uses password authentication, This application will allow the user to Add an event with a Title, Start date/time, Duration, and Description, View Recently added events while also having the option to export your saved event into your mobile device. <br />
+            <br /> Technologies used: Handlebars, Node.js, Express.js, MySQL, ICS package, CSS, JavaScript, Travis and Passport.
           </p>
-          <a href="#" class="btn">Buy Now</a>
+          <a href="#" class="btn">View Project</a>
         </div>
       </section>
 
       <section class="card" data-aos="fade-right">
-        <img src="https://i.ibb.co/0VywMkW/paint-3.png" alt="" />
+        <img src={(BudgetControl)} alt="" />
         <div>
-          <h3>Acrylic Painting Three</h3>
+          <h3>Budget Control</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
             enim laudantium, animi veniam libero eveniet culpa unde perferendis
             illo fugit corporis, voluptatibus totam dolorum, maiores magnam
             officia. Ab, delectus.
           </p>
-          <a href="#" class="btn">Buy Now</a>
+          <a href="#" >View Project</a>
         </div>
       </section>
 
       <section class="card" data-aos="fade-left">
-        <img src="https://i.ibb.co/5LkJFRP/paint-4.png" alt="" />
+        <img src={(Fitness)} alt="" />
         <div>
-          <h3>Acrylic Painting Four</h3>
+          <h3>Fitness Tracking</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
             enim laudantium, animi veniam libero eveniet culpa unde perferendis
             illo fugit corporis, voluptatibus totam dolorum, maiores magnam
             officia. Ab, delectus.
           </p>
-          <a href="#" class="btn">Buy Now</a>
+          <a href="#">View Project</a>
         </div>
       </section>
 
       <section class="card" data-aos="fade-right">
-        <img src="https://i.ibb.co/61R8Q2y/paint-5.png" alt="" />
+        <img src={(passwordGen)} alt="" />
         <div>
-          <h3>Acrylic Painting Five</h3>
+          <h3>Secure Password Generator</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
             enim laudantium, animi veniam libero eveniet culpa unde perferendis
             illo fugit corporis, voluptatibus totam dolorum, maiores magnam
             officia. Ab, delectus.
           </p>
-          <a href="#" class="btn">Buy Now</a>
+          <a href="#">View Project</a>
+        </div>
+      </section>
+
+      <section class="card" data-aos="fade-left">
+        <img src={(CodingQuiz)} alt="" />
+        <div>
+          <h3>Coding Quiz</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+            enim laudantium, animi veniam libero eveniet culpa unde perferendis
+            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+            officia. Ab, delectus.
+          </p>
+          <a href="#">View Project</a>
         </div>
       </section>
     </main>
