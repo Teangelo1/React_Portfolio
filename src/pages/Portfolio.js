f1416279baa0f7afef0fda38a9d5f8f51293189f
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-30 14:01:43 -0400

    Updated description for each project

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -22,12 +22,10 @@ export default function Portfolio() {
       <section class="card">
         <img src={(restaurant)} alt="" />
         <div>
-          <h3>Fooood!</h3>
+          <h3>Feeeed me!</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+          Frontend Application that allows a user to find restaurants in their area based off a zip code/radius search with the option to favorite resturants that you may want to visit frequently. <br />
+            <br /> Technologies Used: HTML, CSS, JavaScript, jQuery, Ajax, Foundation, Yelp API, and Google Maps API
           </p>
           <a href="https://teangelo1.github.io/restaurant-dashboard/" target="#" class="btn">View Project</a>
         </div>
@@ -38,8 +36,8 @@ export default function Portfolio() {
         <div>
           <h3>Amnesia(Null)</h3>
           <p>
-          An Application that uses password authentication, This application will allow the user to Add an event with a Title, Start date/time, Duration, and Description, View Recently added events while also having the option to export your saved event into your mobile device. <br />
-            <br /> Technologies used: Handlebars, Node.js, Express.js, MySQL, ICS package, CSS, JavaScript, Travis and Passport.
+          Full-Stack application that uses password authentication, This application will allow the user to Add an event with a Title, Start date/time, Duration, and Description, View Recently added events while also having the option to export your saved event into your mobile device. <br />
+            <br /> Technologies used: Handlebars, Node.js, Express.js, MySQL, ICS package, CSS, JavaScript, Travis and Passport
           </p>
           <a href="https://agile-gorge-53521.herokuapp.com/members" target="#" class="btn">View Project</a>
         </div>
@@ -50,10 +48,10 @@ export default function Portfolio() {
         <div>
           <h3>Budget Control</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+          Full-Stack application that allows the user to keep track of how much money is being saved or spent. <br />
+
+          <br />Technologies Used: HTML/CSS, JavaScript, Chart.js, Node.js, Express.js, MongoDB/Mongoose, Mongo Atlas and Heroku
+
           </p>
           <a href="https://pure-taiga-35486.herokuapp.com/" target="#" class="btn">View Project</a>
         </div>
@@ -64,10 +62,8 @@ export default function Portfolio() {
         <div>
           <h3>Fitness Tracking</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+           Full-Stack Application that allows the user to create or continue workouts and Track previous history.<br />
+           Technologies Used: HTML/CSS, JavaScript, Node.js, Chart.js, Express.js, MongoDB/Mongoose, Mongo Atlas, and Heroku
           </p>
           <a href="https://warm-journey-95857.herokuapp.com/?id=6071b569a2c2a60015b11168" target="#" class="btn">View Project</a>
         </div>
@@ -78,10 +74,8 @@ export default function Portfolio() {
         <div>
           <h3>Secure Password Generator</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+          Frontend application that will generate a secure password between 8-128 characters giving the user the option to either add an uppercase or lowercase letter, number, and symbol. <br />
+            <br /> Technologies Used: HTML, CSS, JavaScript
           </p>
           <a href="https://teangelo1.github.io/Fun_with_Password_Generator/" target="#" class="btn">View Project</a>
         </div>
@@ -92,10 +86,8 @@ export default function Portfolio() {
         <div>
           <h3>Coding Quiz</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
-            enim laudantium, animi veniam libero eveniet culpa unde perferendis
-            illo fugit corporis, voluptatibus totam dolorum, maiores magnam
-            officia. Ab, delectus.
+          Frontend Timed Quiz application to test your JavaScript Knowledge and generate a score at the end based off of how many questions were answered correctly. <br /> 
+            <br /> Technologies Used: HTML, CSS, JavaScript
           </p>
           <a href="https://teangelo1.github.io/Coding-Quiz/" target="#" class="btn">View Project</a>
         </div>
